ecb902d98b8bdc32113d0336d721c5c0162611e1
Comander227 <comander227@gmail.com>
2025-02-26 17:49:36 -0500

    Various Updates
    Updates to Monster Truck Text file, organization of text files, and including dependencies in the workspace.

---
+++ MCA Workspace/Text Files/Monster Truck JS Code.ts
@@ -0,0 +1,55 @@
+//Setting Tilemap
+tiles.setTilemap(tilemap`level1`);
+
+//Turning off walls for all Acid Tiles
+for (let value of tiles.getTilesByType(assets.tile`acid`)) {
+    tiles.setWallAt(value, false);
+};
+
+//Setting Background Image
+scene.setBackgroundImage(assets.image`background`);
+
+//Establishing Player Sprite
+//Check the assets and pick your truck by replacing # in `truck#`!!!
+let truck = sprites.create(assets.image`truck#`, SpriteKind.Player);
+
+//Create Gravity using ay
+truck.ay = 500;
+
+//Setting forward movement speed
+truck.vx = 100;
+
+//Keep truck in screen
+scene.cameraFollowSprite(truck);
+
+//Establishing Animation for Truck
+animation.runImageAnimation(truck, assets.animation`truck3 animated`, 100, true);
+    
+
+//Jump Controls on "A" Pressed
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    truck.vy = -200
+    music.play(music.melodyPlayable(music.knock), music.PlaybackMode.InBackground)
+});
+
+//Go Button to Restart Movement if Stopped
+controller.right.onEvent(ControllerButtonEvent.Pressed, function (){
+    truck.vx = 100;
+});
+
+//Win Game Condition of making to the end of the tunnal
+scene.onOverlapTile(SpriteKind.Player, assets.tile`empty cave`, function (sprite, location) {
+    game.over(true);
+});
+
+//Lose Game Condition of hitting spikes
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes`, function (sprite, location) {
+    game.over(false);
+});
+
+//Lose Game Condition of hitting acid
+scene.onOverlapTile(SpriteKind.Player, assets.tile`acid`, function (sprite, location) {
+    game.over(false);
+});
+
+
